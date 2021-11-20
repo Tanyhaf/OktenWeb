775eec499a4022aa510f6fb2b2df0813060710ab
tania <taniafesun@gmail.com>
2021-11-20 20:37:40 +0200

    done

--- js_oktw/HomeTasks/Hometask3.js
+++ js_oktw/HomeTasks/Hometask3.js
@@ -100,10 +100,11 @@
         //     console.log(array[i])
         // }
 // - Створити цикл for на 10  ітерацій з кроком 1. Вивести поточний номер кроку через console.log та document.write
-        // for (let i = 0; i < 10; i++) {
-        //     console.log(i)
-        //     document.write(i)
-        // }
+//         for (let i = 0; i < 10; i++) {
+//             console.log(i)
+//             document.write(i)
+//         }
+
 // - Створити цикл for на 100 ітерацій з кроком 1. Вивести поточний номер кроку через console.log та document.write
         // for (let i = 0; i < 100; i++) {
         //     console.log(i)
